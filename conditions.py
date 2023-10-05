2cd9e7f9eefd1d60ca438d7f93c65aaf0b461131
zach-ctrl <146124415+zach-ctrl@users.noreply.github.com>
2023-10-05 15:24:21 -0400

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,34 @@
+ #CS104
+ #Zachary Zucconi
+ #conditions.py
+
+temp=0
+ #print(temp)
+a = int(input("Please enter the current temperature: ")
+        
+ if (a > 90)
+    print("Wear shorts")
+ elif (a > 70)
+    print("Short sleeves are fine")
+
+ if (a >= 70 or a <= 90)
+    print ("Short sleeves are fine") 
+
+ if (a > 50)
+     print("Wear a jacket")
+
+ if (a > 32)
+     print("Wear a heavy coat")
+
+ if (a >= 50 or a <= 32)
+     print("Wear a heavy coat")
+ elif (a < 32)
+     print("Stay Inside")
+     
+ 
+ 
+ 
+ 
+
+    
+
